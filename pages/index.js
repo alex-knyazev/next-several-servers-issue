72adf50e5820845fa0b7522ce1e5152373cbcae2
Alexander Knyazev <alex.knyazev.world@gmail.com>
2022-10-24 20:54:27 +0300

    use local version of next project to test fix

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,11 @@ import style from './style.module.css'
 const Home = () => (
   <div>
     <h1>Welcome!</h1> 
-    <p>Development is available on several hosts. But "Fast refresh" works only on the first opened host.</p>
-    <p>There is a fix for the case with several hosts:</p>
+    <p>Development is available on several hosts. And "Fast refresh" works in each host if ou followed steps below.</p>
     <ol className={style.list}>
       <li>Clone <a href="https://github.com/alex-knyazev/next.js/tree/fast-refresh-for-several-hosts">next.js repo fork</a> in the same folder where the current repo is located.</li> 
       <li>In cloned repo go to branch <a href="https://github.com/alex-knyazev/next.js/tree/fast-refresh-for-several-hosts">fast-refresh-for-several-hosts</a></li>
       <li>Run next.js for development using <a href="https://github.com/vercel/next.js/blob/canary/contributing/core/developing.md">contributing instruction</a></li>
-      <li>In current repo go to branch "use-local-version-with-fix"</li>
-      <li>Run `pnpm dev` again, open several hosts by links below and see that fast refresh work in each browser tab.</li>
     </ol>
     <ul className={style.list}>
       <li><a href="http://localhost:3000">http://localhost:3000</a></li>
